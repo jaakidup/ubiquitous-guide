14ac34024c956d813e5e884a725e9c53f5b5251e
jaakidup <jaakidup@gmail.com>
2019-12-15 20:48:50 +0200

    CORS Support

--- api_server/main.go
+++ api_server/main.go
@@ -58,6 +58,11 @@ func Logger(handle httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 		start := time.Now()
 
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
 		handle(w, r, params)
 
 		log.Printf(
